808571febbe2b00e560729efc5f11394bd2e279f
Landon Byron <lbyron@ngispro.com>
2024-11-12 11:53:34 -0500

    removed the popup on new projects for backups

--- src/routes/(console)/bottomAlerts.ts
+++ src/routes/(console)/bottomAlerts.ts
@@ -5,26 +5,26 @@ import BackupsLight from '$lib/images/backups/promo/backups-light.png';
 import { type BottomModalAlertItem, showBottomModalAlert } from '$lib/stores/bottom-alerts';
 
 const listOfPromotions: BottomModalAlertItem[] = [
-    {
-        id: 'modal:databaseBackups',
-        src: {
-            dark: BackupsDark,
-            light: BackupsLight
-        },
-        title: 'Database Backups are available now',
-        message: 'Protect your data and ensure quick recovery with our new backups',
-        plan: 'pro',
-        scope: 'project',
-        importance: 8,
-        cta: {
-            text: 'Try now',
-            link: ({ project }) => `${base}/project-${project.$id}/databases`
-        },
-        learnMore: {
-            text: 'Learn more',
-            link: () => 'http://appwrite.io/docs/products/databases/backups'
-        }
-    }
+    // {
+    //     id: 'modal:databaseBackups',
+    //     src: {
+    //         dark: BackupsDark,
+    //         light: BackupsLight
+    //     },
+    //     title: 'Database Backups are available now',
+    //     message: 'Protect your data and ensure quick recovery with our new backups',
+    //     plan: 'pro',
+    //     scope: 'project',
+    //     importance: 8,
+    //     cta: {
+    //         text: 'Try now',
+    //         link: ({ project }) => `${base}/project-${project.$id}/databases`
+    //     },
+    //     learnMore: {
+    //         text: 'Learn more',
+    //         link: () => 'http://appwrite.io/docs/products/databases/backups'
+    //     }
+    // }
 ];
 
 export function addBottomModalAlerts() {
